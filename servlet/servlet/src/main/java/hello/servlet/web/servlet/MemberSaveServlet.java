ff8686e77c668ae5e838291d22c0e5d00a64ccc9
younlll <youn.lee96@gmail.com>
2022-07-19 02:31:38 +0900

    feat: Member 저장 로직

--- servlet/servlet/src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ servlet/servlet/src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -1,5 +1,6 @@
 package hello.servlet.web.servlet;
 
+import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,29 +10,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
-public class MemberFormServlet extends HttpServlet {
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
         response.setContentType("text/html");
         response.setCharacterEncoding("utf-8");
-
         PrintWriter w = response.getWriter();
-        w.write("<!DOCTYPE html>\n" +
-                "<html>\n" +
+        w.write("<html>\n" +
                 "<head>\n" +
                 " <meta charset=\"UTF-8\">\n" +
-                " <title>Title</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
-                " username: <input type=\"text\" name=\"username\" />\n" +
-                " age: <input type=\"text\" name=\"age\" />\n" +
-                " <button type=\"submit\">전송</button>\n" +
-                "</form>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+member.getId()+"</li>\n" +
+                " <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
                 "</body>\n" +
-                "</html>\n");
+                "</html>");
     }
 }
